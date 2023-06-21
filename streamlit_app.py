1f37794fc75f8dbfba4fc7cc5a7f838f22ec09d8
David Faola <82265740+dfaola@users.noreply.github.com>
2023-06-21 12:00:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # insert syntax
-my_cur.execute("insert into fruit_load_list values ('test streamlit')")
+my_cur.execute("insert into fruit_load_list values ('orange')")
 
 
 streamlit.header("Fruityvice Fruit Advice!")
